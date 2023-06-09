8d8b786aa29f59070303144f13b58bad3de4900b
MathiasChirivi <chirivimathias3@gmail.com>
2023-06-09 15:34:07 +0200

    layout form

--- formAsk.php
+++ formAsk.php
@@ -6,8 +6,66 @@
     <title>php-strong-password-generator</title>
     <!-- add bootstrap -->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
-<body>
+
+<body >
+
+    <header>
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-auto m-auto p-4">
+                    <h2 class="text-white">Strong Password Generator</h2>
+                    <h5 class="text-white">Genera una password sicura</h5>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="col-12">
+                <div class="container-fluid bg-info-subtle">
+                    <div class="p-3">
+                        Nessun parametro valido
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container mt-3">
+            <div class="col-12">
+                <div class="container-fluid bg-white p-3">
+                    <div class="d-flex flex-column">
+                        <form action="formAsk.php" method="get">
+                                <label for="">Lunghezza password:</label>
+                                <input type="text" name="passLength">
+                        </form>
+                        <form class="mt-4" action="formAsk.php" method="get">
+                            <div class="d-flex">
+                                <label for="">Consenti ripetizioni di uno o piu caratteri:</label>
+                                <div class="d-flex flex-column ms-5">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                        <label class="form-check-label" for="flexRadioDefault1">
+                                            si
+                                        </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+                                        <label class="form-check-label" for="flexRadioDefault2">
+                                            no
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-5">Invia</button>
+                            <button type="submit" class="btn btn-secondary mt-5">Annulla</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
     
 
 <!-- Add js Bootstrap -->
